a3633405070decb3d48b9f13b14e81ccd99c7f6f
CThaw90 <cthaw@mediaocean.com>
2016-07-11 15:42:13 -0400

    Create grunt task to handle zipping files for build

---
+++ Gruntfile.js
@@ -0,0 +1,28 @@
+/*global require, module */
+module.exports = function (grunt) {
+    'use strict';
+    
+    require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
+    
+    grunt.initConfig({
+        gruntConfig: {},
+        pkg: grunt.file.readJSON('package.json'),
+        zip_directories: {
+            'dist': {
+                files: [
+                    {
+                        filter: 'isDirectory',
+                        expand: true,
+                        src: ['*'],
+                        dest: './dist'
+                    }
+                ]
+            }
+        },
+        clean: {
+            zip: ['dist']
+        }
+    });
+    
+    grunt.registerTask('default', ['zip_directories:dist']);
+};
